95ad9de0b3b159795afe34fe6a7b4c06865babb4
Jeremy Andy Ampatin <mtonilon17@gmail.com>
2023-05-22 18:58:16 +0800

    added some functions

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
     ['A. ', 'B. Jeremy', 'C. ampotin'],
     ['A. sir grew', 'gabuya ', ' krisha'],
     ['andrian ', 'paul', 'sedigo'],
-    ['Choice 1A', 'Choice 1B', 'Choice 1C'],
+    ['Choice 1A', 'Choice 1B', 'Choice 1C', 'Choice 1D'],
     ['Choice 2A', 'Choice 2B', 'Choice 2C'],
     ['Choice 3A', 'Choice 3B', 'Choice 3C'],
     ['Choice 1A', 'Choice 1B', 'Choice 1C'],
@@ -53,8 +53,6 @@ class _HomePageState extends State<HomePage> {
     'What is your dream vacation destination?',
     'Who is your favorite artist?',
     'What is your favorite color?',
-
-    // Add more text questions here
   ];
 
   List<int> selectedAnswerIndices = List.generate(20, (index) => -1);
@@ -65,73 +63,173 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('QuizChamp'),
       ),
-      body: ListView.builder(
-        itemCount: itemList.length,
+      body: ListView.separated(
+        itemCount: itemList.length + 1,
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            color: Colors.black,
+            height: 20,
+            thickness: 2,
+          );
+        },
         itemBuilder: (BuildContext context, int index) {
-          final String item = itemList[index];
-          final List<String> choices = choicesList[index];
-          final int selectedAnswerIndex = selectedAnswerIndices[index];
-
-          return ListTile(
-            title: Row(
-              children: [
-                Expanded(
-                  child: Text(item),
-                ),
-                Text(
-                  selectedAnswerIndex != -1
-                      ? 'Answer: ${choices[selectedAnswerIndex]}'
-                      : '',
-                  style: TextStyle(
-                    fontStyle: selectedAnswerIndex != -1
-                        ? FontStyle.italic
-                        : FontStyle.normal,
-                  ),
-                ),
-              ],
-            ),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text(item),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(textQuestions[index]), // Display the text question
-                        const SizedBox(height: 10),
-                        ...List.generate(choices.length, (choiceIndex) {
-                          return RadioListTile(
-                            title: Text(choices[choiceIndex]),
-                            value: choiceIndex,
-                            groupValue: selectedAnswerIndex,
-                            onChanged: (int? value) {
-                              setState(() {
-                                selectedAnswerIndices[index] = value!;
-                              });
-                            },
-                          );
-                        }),
-                      ],
-                    ),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          // Handle answer submission here
-                          print('Question: $item');
-                          print(
-                              'Selected answer index: ${selectedAnswerIndices[index]}');
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Submit'),
-                      ),
-                    ],
+          if (index == itemList.length) {
+            return Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Answered History'),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: List.generate(itemList.length, (index) {
+                              final String question = itemList[index];
+                              final List<String> choices = choicesList[index];
+                              final int selectedAnswerIndex =
+                                  selectedAnswerIndices[index];
+                              return ListTile(
+                                title: Text(question),
+                                subtitle: selectedAnswerIndex != -1
+                                    ? Text(
+                                        'Answer: ${choices[selectedAnswerIndex]}')
+                                    : const Text('Not answered'),
+                              );
+                            }),
+                          ),
+                        ),
+                        actions: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Close'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Submit Answers'),
+                                        content: const Text(
+                                            'Are you sure you want to submit your answers?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Cancel'),
+                                          ),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              // Perform submit action here
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Proceed'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: const Text('Submit'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
-              );
-            },
-          );
+                child: const Text('Show Answered History'),
+              ),
+            );
+          } else {
+            final String item = itemList[index];
+            final List<String> choices = choicesList[index];
+            final int selectedAnswerIndex = selectedAnswerIndices[index];
+
+            return ListTile(
+              title: Row(
+                children: [
+                  Expanded(
+                    child: Text(item),
+                  ),
+                  Text(
+                    selectedAnswerIndex != -1
+                        ? 'Answer: ${choices[selectedAnswerIndex]}'
+                        : '',
+                    style: TextStyle(
+                      fontStyle: selectedAnswerIndex != -1
+                          ? FontStyle.italic
+                          : FontStyle.normal,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    int? selectedChoiceIndex = selectedAnswerIndices[index];
+                    return StatefulBuilder(
+                      builder: (BuildContext context, setState) {
+                        return AlertDialog(
+                          title: Text(item),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(textQuestions[index]),
+                              const SizedBox(height: 10),
+                              ...List.generate(choices.length, (choiceIndex) {
+                                return RadioListTile(
+                                  title: Text(choices[choiceIndex]),
+                                  value: choiceIndex,
+                                  groupValue: selectedChoiceIndex,
+                                  onChanged: (int? value) {
+                                    setState(() {
+                                      selectedChoiceIndex = value;
+                                    });
+                                  },
+                                );
+                              }),
+                            ],
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                if (selectedChoiceIndex != null) {
+                                  setState(() {
+                                    selectedAnswerIndices[index] =
+                                        selectedChoiceIndex!;
+                                  });
+                                }
+                                print('Question: $item');
+                                print(
+                                    'Selected answer index: ${selectedAnswerIndices[index]}');
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Submit'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ).then((_) {
+                  setState(() {}); // Update the UI after closing the dialog
+                });
+              },
+            );
+          }
         },
       ),
     );
